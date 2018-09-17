8af91955202796c50ad8628bf02ac0c3379b793f
Kshitij Lingthep <klingthe@go.olemiss.edu>
2018-09-16 21:20:27 -0500

    second step

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
 
+ showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Alert!',
+      subTitle: 'The app is not yet completed. You have to wait!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 }
+
